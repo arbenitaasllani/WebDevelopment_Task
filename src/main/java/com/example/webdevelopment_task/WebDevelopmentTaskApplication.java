ee695688ca574d29303e9009a9bd1883287913dd
Arbenita Asllani <arbenite.asllani@student.uni-pr.edu>
2023-12-01 12:01:34 +0100

    Initial commit

---
+++ src/main/java/com/example/webdevelopment_task/WebDevelopmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopmentTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevelopmentTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdevelopment_task/WebDevelopmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
